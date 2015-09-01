ced8cbea35fc6bf7a7640ff9c2a0f50a14a3566d
Sebastien Saunier <seb@saunier.me>
2015-09-01 14:47:15 +0200

    HTTPS queries

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -3,7 +3,7 @@ Geocoder.configure(
   # :timeout      => 3,           # geocoding service timeout (secs)
   :lookup       => :google,     # name of geocoding service (symbol)
   # :language     => :en,         # ISO-639 language code
-  # :use_https    => false,       # use HTTPS for lookup requests? (if supported)
+  :use_https    => true,       # use HTTPS for lookup requests? (if supported)
   # :http_proxy   => nil,         # HTTP proxy server (user:pass@host:port)
   # :https_proxy  => nil,         # HTTPS proxy server (user:pass@host:port)
   :api_key      => ENV["GOOGLE_SERVER_API_KEY"],         # API key for geocoding service
