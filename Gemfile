79319f8b44b6939416a1d7a7b44095c4ebaf74ca
Sébastien Saunier <seb@saunier.me>
2020-10-12 10:39:20 +0200

    Bump Ruby + Dependencies

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "https://rubygems.org"
 
-ruby "2.5.1"
+ruby "2.6.6"
 gem "rails", "5.0.5"
 gem "pg"
 gem "puma"
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -16,7 +15,7 @@ ActiveRecord::Schema.define(version: 20140523131531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "flats", force: true do |t|
+  create_table "flats", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
     t.datetime "created_at"
